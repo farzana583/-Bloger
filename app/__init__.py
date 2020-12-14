8558df3ecd306af981a612940b39ee19b9dd01c8
farzana583 <farzanaisack@gmail.com>
2020-12-14 15:07:57 +0300

    add

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,45 @@
 from flask import Flask
-
-app = Flask(__name__)
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
+from config import config_options
 from flask_login import LoginManager
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'farnaz'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-login_manager.login_message_category = 'info'
+from flask_uploads import UploadSet, configure_uploads,IMAGES
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from flask_simplemde import SimpleMDE
 
 
-from app import routes
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+simple = SimpleMDE()
+
+db = SQLAlchemy()
+mail = Mail()
+bootstrap = Bootstrap()
+photos = UploadSet('photos',IMAGES)
+
+def create_app(config_name):
+    app = Flask(__name__)
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+
+    # Initializing flask extensions
+    db.init_app(app)
+    mail.init_app(app)
+    bootstrap.init_app(app)
+    login_manager.init_app(app)
+    simple.init_app(app)
+
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
+
+
