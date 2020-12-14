a0e09155c820e90f78c7ee1d496a1a44f6d814f8
farzana583 <farzanaisack@gmail.com>
2020-12-14 15:21:11 +0300

    add

--- config.py
+++ config.py
@@ -1,6 +1,32 @@
 import os
 
-
 class Config:
+
+  
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:farzana123@localhost/farnaz'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+#  email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME ='farzanaisaak@gmail.com'
+    MAIL_PASSWORD = 'Buley2241'
+    SENDER_MAIL = 'farzanaisaak@gmail.com'
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    pass
+
+
+class DevConfig(Config):
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+}
